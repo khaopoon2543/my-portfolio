9f0e5eebffb842e05c19a4a2a050c978ffdbca04
khaopoon2543 <khaopoon2543@gmail.com>
2022-10-14 16:02:59 +0700

    edit project Gif, hover skill, change profile me

--- src/component/projects/DataProjects.js
+++ src/component/projects/DataProjects.js
@@ -1,7 +1,9 @@
 const DataProjects = [
     {
         name: "Kashify",
-        image: "kashify.gif",
+        name2: "Kashify",
+        image: "kashify.jpg",
+        gif: "kashify.gif",
         description: `
                         Web Application learning Japanese words through lyrics. Users can quickly and easily learn the Thai and English meanings of Japanese terms in the lyrics along with listening Japanese songs.
                         This Project, I designed interface by AdobeXD, scrapping data by Beautifulsoup and Selenium, tokenization and part of speech tagging in Japanese by Python, used MongoDB as database and developed by ReactJs, ExpressJs, Bootstrap.
@@ -13,7 +15,9 @@ const DataProjects = [
     },
     {
         name: "My Gallery",
-        image: "mygallery.gif",
+        name2: "Mygallery",
+        image: "mygallery.jpg",
+        gif: "mygallery.gif",
         description: "The website collects my lovely paintings and information about my commission. Developed by ReactJs and Bootstrap.",
         github_link: "https://github.com/khaopoon2543/My-Galllery",
         demo_link: "https://kaopuny.netlify.app/",
@@ -21,27 +25,34 @@ const DataProjects = [
     },
     {
         name: "Language And Information Technology Website (demo)",
-        image: "langtech.gif",
+        name2: "Langtech",
+        image: "langtech.jpg",
+        gif: "langtech.gif",
         description: "This project is  the department of Language and Information Technology website that redesigned form currently website. In this project, I interviewed user for analysis users and system requirement, designed new interface by Figma and developed website by Wordpress.",
         prototype_link: "https://www.figma.com/file/7Moxgn6WiHTWRfCZLPB7Vm/Website-Langtech?node-id=0%3A1",     
         tools: ["Wordpress","Figma"]     
     },
     {
         name: "Attending System (demo)",
-        image: "attendingsystem.gif",
+        name2: "Attendingsystem",
+        image: "attendingsystem.jpg",
+        gif: "attendingsystem.gif",
         description: "Web application adding request form in Chulalongkorn University's Faculty of Arts, used by student, teacher and officer. In this project, I interview users for analysis user and system requirement, designed interface by AdobeXD and developed by PHP and MySQL.",
         github_link: "https://github.com/khaopoon2543/Attending-System-Project",   
         tools: ["PHP","MySQL","AdobeXD"]     
     },
     {
         name: "Questionnaire Headline",
-        image: "headlineforstat.gif",
+        name2: "Headlineforstat",
+        image: "headlineforstat.jpg",
+        gif: "headlineforstat.gif",
         description: "Web application questionnaire about the headline that caught the eye most of the respondents. In this project developed by PHP MySQL and Bootstrap.",
         github_link: "https://github.com/khaopoon2543/Headline-For-Stat",   
         tools: ["PHP","MySQL","Bootstrap"]     
     },
     {
         name: "Prototype Shopee (study)",
+        name2: "Shopee",
         image: "shopee.jpg",
         description: "Cloning Shopee interface for studying UX&UI and Figma.",
         prototype_link: "https://www.figma.com/file/tmWMQ4nJ6VG2sXgZOobN7i/Shopee?node-id=0%3A1",
--- src/component/projects/Projects.js
+++ src/component/projects/Projects.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './projects.css'
 import {DataProjects} from "./DataProjects";
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 
 function Tools(items) {
     const TOOLS = items.map((item) => {
@@ -10,10 +11,34 @@ function Tools(items) {
 }
 
 function AllProjects(projects) {
+    const [isPlayGif, setIsPlayGif] = useState("");
+
+    function isPlaying(isPlayGif,name) {
+        if (isPlayGif==="") {        
+            return setIsPlayGif(name)    
+        } else if (isPlayGif!=="" && isPlayGif!==name) {
+            return setIsPlayGif(name)
+        } else if (isPlayGif===name) {
+            return setIsPlayGif("")
+        }
+    }
+
     const PROJECTS = projects.map((project) => {
         return  <article class="project_item">
-                    <div className="project_item-image">
-                        <img src={require(`../../assets/${project.image}`)} alt="" />
+                    <div className="project_item-image" onClick={() => isPlaying(isPlayGif,project.name2)}>
+                        {project.gif ?
+                        <>
+                            <img src={require(`../../assets/${(isPlayGif!==project.name2) ? project.image : project.gif}`)} alt="" />
+                            {(isPlayGif!==project.name2) ? 
+                                <button class="btn play"><BsFillPlayFill /></button>
+                            :
+                                <button class="btn pause"><BsFillPauseFill /></button>
+                            }
+                        </>
+                        :
+                            <img src={require(`../../assets/${project.image}`)} alt="" />
+                        }
+                        
                     </div>
 
                     <div className="project_item-info">
